506920c2ec3efed24d1cedce04e3dcff8562d96b
hongjiaherng <jiaherng2001@gmail.com>
2025-11-13 17:47:17 +0800

    fix: uv cache dir in second drive

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,5 @@ oh-my-posh init pwsh --config $HOME\.oh-my-posh.omp.json | Invoke-Expression
 
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 $env:SHELL = "C:\Program Files\PowerShell\7\pwsh.exe"
+
+$Env:UV_CACHE_DIR = "D:\jherng\System\uv\cache"
